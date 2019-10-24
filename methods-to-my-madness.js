87361a3fb2bf00f4c4490fc6815d369d7a3f1f14
busrasariguzel <busra.sariguzel@codeimmersives.com>
2019-10-24 11:52:55 -0400

    methods to my madness added

--- methods-to-my-madness.js
+++ methods-to-my-madness.js
@@ -13,7 +13,7 @@ function repeat(str, repetitions) {
   
   let count = 1;
   while (count <= repetitions) {
-    repeated + str;
+    repeated = repeated + str;
     count++;
   }
   
@@ -25,18 +25,20 @@ function startsWith(string, substring) {
     if (string[i] !== substring[i]) {
       return false;
     }
+    return true;
   }
 
-  return true;
+  
 }
 
-function endsWith(str) {
+function endsWith(str,substring) {
   for (let i = 0; i <= substring.length; i++) {
     const substringIndex = substring.length - i;
-    const strIndex = str.length - i;
+    const strIndex = str.length- i;
     if (str[strIndex] !== substring[substringIndex]) {
       return false;
     }
+    
   }
 
   return true;
@@ -44,19 +46,21 @@ function endsWith(str) {
 
 function includes(arr, item) {
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== item) {
+    if (arr[i] === item) {
       return true;
     }
+    
   }
 
   return false;
 }
 
 function join(arr, separator = '') {
-  let str = arr[0];
+  let str= arr[0];
   for (let i = 1; i < arr.length; i++) {
     str += separator + arr[i];
   }
+  return str
 }
 
 
@@ -66,9 +70,11 @@ function split(str, separator) {
 
   for (let i = 0; i < str.length; i++) {
     if (str[i] === separator) {
-      arr.push(str.slice(strStartIndex, i));
-      strStartIndex = i;
+      arr.push(str.slice(strStartIndex,i));
+      strStartIndex = i+1;
+      
     }
+
   }
 
   arr.push(str.slice(strStartIndex));
@@ -80,9 +86,10 @@ function trimStart(str) {
   let trimmed = '';
 
   for (let i = 0; i < str.length; i++) {
-    if (str[i] !== '') {
+    if (str[i] !== ' ') {
       return str.slice(i);
     }
+    
   }
 
   return trimmed;
